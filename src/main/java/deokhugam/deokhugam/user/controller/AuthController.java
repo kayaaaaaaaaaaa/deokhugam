13c79d79656cb83304c68b1d0852e66ad13bb23e
kaya <kaya_frog_ramer@kakao.com>
2025-12-30 22:36:17 +0900

    feat: AuthController 구현

---
+++ src/main/java/deokhugam/deokhugam/user/controller/AuthController.java
@@ -0,0 +1,31 @@
+package deokhugam.deokhugam.user.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import deokhugam.deokhugam.user.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+	private final AuthService authService;
+
+	@PostMapping("/login")
+	public ResponseEntity<LoginResponse> login(
+		@RequestBody @Valid LoginRequest request
+	) {
+		LoginResponse response = authService.login(
+			request.getEmail(),
+			request.getPassword()
+		);
+
+		return ResponseEntity.ok(response);
+	}
+}
